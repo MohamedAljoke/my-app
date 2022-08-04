f6776540c68f154e2e642c04346bcca30d8e1e70
Janku13 <94984384+Janku13@users.noreply.github.com>
2022-08-03 21:27:45 -0300

    interface

--- src/App.js
+++ src/App.js
@@ -1,23 +1,126 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const defaultValues = {
+    pulo: 5,
+    queda: 3,
+    pauseInMillSecond: 1000, //1sec
+    fundoColor: 'red',
+    metadeColor: 'yellow',
+    finalColor: 'green',
+  };
+  const [formData, setFormData] = useState({
+    profundidade: 0,
+    passo: 0,
+    queda: 0,
+  });
+  const [userInputFeedBack, setUserInfoFeedBack] = useState('');
+  const [boxColor, setBoxColor] = useState(defaultValues.fundoColor);
+  const pause = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setFormData((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  }
+  async function onSubmitJogar(event) {
+    event.preventDefault();
+    setBoxColor(defaultValues.fundoColor);
+    setUserInfoFeedBack('');
+    const { profundidade, passo, queda } = formData;
+    //mudando inputs para str
+    let intProfu = parseInt(profundidade);
+    let intQueda = parseInt(queda);
+    //se não tem como sair
+    if (profundidade > passo - queda) {
+      setUserInfoFeedBack('A minhoquinha não consegue sair');
+    } else {
+      let safeExit = 0;
+      let numeroDePulos = 0;
+      do {
+        numeroDePulos++;
+        intProfu = intProfu - defaultValues.pulo;
+        // verificar se tem queda e então adicionar a queda para profu do buraco
+        intProfu = intQueda > 0 ? intProfu + defaultValues.queda : intProfu;
+        //pausa caso teve queda
+        if (intQueda > 0) {
+          await pause(defaultValues.pauseInMillSecond);
+        }
+        intQueda = intQueda > 0 ? intQueda - defaultValues.queda : 0;
+        // intPasso = intPasso - defaultValues.pulo;
+
+        if (parseInt(profundidade) / 2 < intProfu) {
+          setBoxColor(defaultValues.metadeColor);
+          setUserInfoFeedBack('metade');
+        }
+
+        // console.log({ intQueda });
+        if (safeExit > 1000000 || safeExit < -1000000) {
+          break;
+        }
+      } while (intProfu > 0);
+      console.log(numeroDePulos);
+      setUserInfoFeedBack(`Foi ${numeroDePulos} pulos`);
+      setBoxColor(defaultValues.finalColor);
+    }
+  }
+  const inputData = [
+    {
+      id: 'profundidade',
+      labelText: 'Informe a profundidade pf',
+    },
+    {
+      id: 'passo',
+      labelText: 'Quantos passos?',
+    },
+    {
+      id: 'queda',
+      labelText: 'Quantos quedas?',
+    },
+  ];
+  const containerStyle = {
+    display: 'grid',
+    width: '200px',
+    marginBottom: '30px',
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <div style={{ display: 'grid', placeItems: 'center' }}>
+          <form onSubmit={onSubmitJogar}>
+            {inputData.map((input) => {
+              return (
+                <div style={containerStyle}>
+                  <label htmlFor={input.id}>{input.labelText}</label>
+                  <input
+                    min={input.minValue}
+                    id={input.id}
+                    type="0"
+                    name={input.id}
+                    onChange={handleChange}
+                  />
+                </div>
+              );
+            })}
+
+            <button type="submit">Jogar</button>
+          </form>
+          <div
+            style={{
+              width: '300px',
+              height: '300px',
+              backgroundColor: `${boxColor}`,
+              marginTop: '20px',
+            }}
+          >
+            {userInputFeedBack}
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
